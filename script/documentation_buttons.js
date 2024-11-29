68ba71917f3cd418c8f1c9d1e207753c710ca643
Konstantinos Gkotzamanidis <konstantinosgkotzamanidis@outlook.com>
2024-11-29 19:45:09 +0200

    adding documentetion!

--- script/documentation_buttons.js
+++ script/documentation_buttons.js
@@ -1,17 +1,64 @@
-const Winbutton = document.getElementById('WinBtn');
-const LinButton = document.getElementById('LinuxBtn');
-const output_update = document.getElementById('output_UpdateOS');
+// Take button's element
+const WindowsBtn = document.getElementById('WinBtn');
+const DebianBtn = document.getElementById('DebianBtn');
+const ArchBtn = document.getElementById("ArchBtn");
 
-Winbutton.addEventListener('click', () => {
-    const codeElement = document.createElement('code');
-    codeElement.textContent = 'test code by javascript';
-    output_update.innerHTML = '';
-    output_update.appendChild(codeElement);
+// Create Element
+const codeElement = document.createElement('code');
+const textElement = document.createElement('text');
+
+// Append text
+const strUpdateOs = document.getElementById('howtoupdateOS');
+const codeUpdateOs = document.createElement('code');
+const strAboutJava = document.getElementById('aboutjava');
+const codeAboutJava = document.createElement('code');
+const strErrorAboutJava = document.getElementById('erroraboutjava');
+const linkJava = document.createElement('a');
+
+WindowsBtn.addEventListener('click', () => {
+    strUpdateOs.innerHTML = 'To take the last Windows update will use the CMD.exe and this Command: ';
+    codeUpdateOs.textContent = 'wuauclt /detectnow /updatenow';
+    strUpdateOs.appendChild(codeUpdateOs);
+
+    strAboutJava.innerHTML = 'First we need to check the Java Version or if the java has been installed on our system, with CMD.exe and this Command: ';
+    codeAboutJava.textContent = 'java --version';
+    strAboutJava.appendChild(codeAboutJava);
+
+    strErrorAboutJava.innerHTML = 'If we get error like is not recognized as an internal or external command, then will need to download from here: ';
+    linkJava.href = 'https://www.oracle.com/java/technologies/downloads/#jdk23-windows';
+    linkJava.textContent = 'Download Java Windows Support';
+    linkJava.target = '_blank';
+    strErrorAboutJava.appendChild(linkJava);
 });
 
-LinButton.addEventListener('click', () => {
-    const codeElement = document.createElement('code');
-    codeElement.textContent = 'pacman -Syyu';
-    output_update.innerHTML = '';
-    output_update.appendChild(codeElement);
+DebianBtn.addEventListener('click', () => {
+    strUpdateOs.innerHTML = 'To take the last Debian-Linux update will use the bash and this Command: ';
+    codeUpdateOs.textContent = 'sudo apt-get update && sudo apt-get install';
+    strUpdateOs.appendChild(codeUpdateOs);
+
+    strAboutJava.innerHTML = 'First we need to check the Java Version or if the java has been installed on our system, with bash and this Command: ';
+    codeAboutJava.textContent = 'java --version';
+    strAboutJava.appendChild(codeAboutJava);
+
+    strErrorAboutJava.innerHTML = 'If we get error like command not found, then will need to download from here: ';
+    linkJava.href = 'https://www.oracle.com/java/technologies/downloads/#jdk23-linux';
+    linkJava.textContent = 'Download Java Linux Support';
+    linkJava.target = '_blank';
+    strErrorAboutJava.appendChild(linkJava);
+});
+
+ArchBtn.addEventListener('click', () => {
+    strUpdateOs.innerHTML = 'To take the last Arch-Linux update will use the bash and this Command: ';
+    codeUpdateOs.textContent = 'sudo pacman -Syyuu';
+    strUpdateOs.appendChild(codeUpdateOs);
+
+    strAboutJava.innerHTML = 'First we need to check the Java Version or if the java has been installed on our system, with bash and this Command: ';
+    codeAboutJava.textContent = 'java --version';
+    strAboutJava.appendChild(codeAboutJava);
+
+    strErrorAboutJava.innerHTML = 'If we get error like command not found, then will need to download from here: ';
+    linkJava.href = 'https://www.oracle.com/java/technologies/downloads/#jdk23-linux';
+    linkJava.textContent = 'Download Java Linux Support';
+    linkJava.target = '_blank';
+    strErrorAboutJava.appendChild(linkJava);
 });
