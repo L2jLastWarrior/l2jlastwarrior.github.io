f222e25734af80149ce6dbc026de42206f1035e2
Konstantinos Gkotzamanidis <konstantinosgkotzamanidis@outlook.com>
2024-12-03 16:12:14 +0200

    update the site

--- script/documentation_buttons.js
+++ script/documentation_buttons.js
@@ -7,135 +7,71 @@ document.getElementById('backtop').addEventListener('click',()=>{
     document.getElementById('select-installation').scrollIntoView({behavior:'smooth'});
 });
 
-document.getElementById('updateos-link').addEventListener('click',()=>{
-    document.getElementById('select-os').scrollIntoView({behavior:'smooth'});
+document.getElementById('java_download_link').addEventListener('click',()=>{
+    document.getElementById('select_java').scrollIntoView({behavior:'smooth'});
 });
 
-document.getElementById('java-link').addEventListener('click',()=>{
-    document.getElementById('select-java').scrollIntoView({behavior:'smooth'});
+document.getElementById('mariadb_download_link').addEventListener('click',()=>{
+    document.getElementById('select_mariadb').scrollIntoView({behavior:'smooth'});
 });
 
-document.getElementById('mariadb-link').addEventListener('click',()=>{
-    document.getElementById('select-mariadb').scrollIntoView({behavior:'smooth'});
-});
-
-document.getElementById('apache-link').addEventListener('click',()=>{
-    document.getElementById('select-apache').scrollIntoView({behavior:'smooth'});
-});
-
-document.getElementById('php-link').addEventListener('click',()=>{
-    document.getElementById('select-php').scrollIntoView({behavior:'smooth'});
-});
-
-document.getElementById('phpmyadmin-link').addEventListener('click',()=>{
-    document.getElementById('select-phpmyadmin').scrollIntoView({behavior:'smooth'});
+document.getElementById('dbeaver_download_link').addEventListener('click',()=>{
+    document.getElementById('select_dbeaver').scrollIntoView({behavior:'smooth'});
 });
 
 // Take button's element
 const WindowsBtn = document.getElementById('WinBtn');
-const DebianBtn = document.getElementById('DebianBtn');
-const ArchBtn = document.getElementById("ArchBtn");
+const LinuxBtn = document.getElementById('LinuxBtn');
 
 // Template Element
 const codeElement = document.createElement('code');
 const textElement = document.createElement('text');
 
-// Append text
-// Update OS
-const strUpdateOs = document.getElementById('howtoupdateOS');
-const codeUpdateOs = document.createElement('code');
-// Java
-const strAboutJava = document.getElementById('aboutjava');
-const codeAboutJava = document.createElement('code');
-const strErrorAboutJava = document.getElementById('erroraboutjava');
+const strHowToJava = document.getElementById('how_to_java');
 const linkJava = document.createElement('a');
-// MariaDB
-const strAboutMariaDB = document.getElementById('aboutmariadb');
-const codeAboutMariaDB = document.createElement('code');
-const strErrorAboutMariaDB = document.getElementById('erroraboutmariadb');
+
+const strHowToMariaDB = document.getElementById('how_to_mariadb');
 const linkMariaDB = document.createElement('a');
-// PHP
-const strAboutPhp = document.getElementById('aboutphp');
-const codeAboutPhp = document.createElement('code');
-const strErrorAboutPhp = document.getElementById('erroraboutphp');
-const linkPhp = document.createElement('a');
-// PhpMyAdmin
-const strAboutPhpMyAdmin = document.getElementById('aboutphpmyadmin');
-const codeAboutPhpMyAdmin = document.createElement('code');
-const strErrorAboutPhpMyAdmin = document.getElementById('erroraboutphpmyadmin');
-const linkPhpMyAdmin = document.createElement('a');
+
+const strHowToDBeaver = document.getElementById('how_to_dbeaver');
+const linkDBeaver = document.createElement('a');
 
 WindowsBtn.addEventListener('click', () => {
-    strUpdateOs.innerHTML = 'To take the last Windows update will use the CMD.exe and this Command: ';
-    codeUpdateOs.textContent = 'wuauclt /detectnow /updatenow';
-    strUpdateOs.appendChild(codeUpdateOs);
-
-    strAboutJava.innerHTML = 'First we need to check the Java Version or if the java has been installed on our system, with CMD.exe and this Command: ';
-    codeAboutJava.textContent = 'java --version';
-    strAboutJava.appendChild(codeAboutJava);
-    strErrorAboutJava.innerHTML = 'If we get error like is not recognized as an internal or external command, then will need to download from here: ';
-    linkJava.href = 'https://www.oracle.com/java/technologies/downloads/#jdk23-windows';
-    linkJava.textContent = 'Download Java Windows Support';
+    strHowToJava.innerHTML = 'You can download the Java Runtime Environment(JRE) from here: ';
+    linkJava.href = 'https://www.java.com/en/download/help/windows_manual_download.html';
     linkJava.target = '_blank';
-    strErrorAboutJava.appendChild(linkJava);
+    linkJava.textContent = 'Download for Windows';
+    strHowToJava.appendChild(linkJava);
 
-    strAboutMariaDB.innerHTML = 'First we need to check the MariaDB Version or if the MariaDB has been installed on our system, with CMD.exe and this Command: ';
-    codeAboutMariaDB.textContent = 'mysql --version';
-    strAboutMariaDB.appendChild(codeAboutMariaDB);
-    strErrorAboutMariaDB.innerHTML = 'If we get error like is not recognized as an internal or external command, then will need to download from here: ';
-    linkMariaDB.href = 'https://mariadb.org/download/?t=mariadb&p=mariadb&r=11.6.2&os=windows&cpu=x86_64&pkg=msi&mirror=crete';
-    linkMariaDB.textContent = 'Download MariaDB Windows Support';
+    strHowToMariaDB.innerHTML = 'You can download the MariaDB from here: ';
+    linkMariaDB.href = 'https://mariadb.org/download/?t=mariadb&p=mariadb&r=11.4.4&os=windows&cpu=x86_64&pkg=msi&mirror=neterra';
     linkMariaDB.target = '_blank';
-    strErrorAboutMariaDB.appendChild(linkMariaDB);
+    linkMariaDB.textContent = 'Download for Windows';
+    strHowToMariaDB.appendChild(linkMariaDB);
 
+    strHowToDBeaver.innerHTML = 'You can download the DBeaver from here: ';
+    linkDBeaver.href = 'https://dbeaver.io/download/';
+    linkDBeaver.target = '_blank';
+    linkDBeaver.textContent = 'Download for Windows';
+    strHowToDBeaver.appendChild(linkDBeaver);
 });
 
-DebianBtn.addEventListener('click', () => {
-    strUpdateOs.innerHTML = 'To take the last Debian-Linux update will use the bash and this Command: ';
-    codeUpdateOs.textContent = 'sudo apt-get update && sudo apt-get install';
-    strUpdateOs.appendChild(codeUpdateOs);
-
-    strAboutJava.innerHTML = 'First we need to check the Java Version or if the java has been installed on our system, with bash and this Command: ';
-    codeAboutJava.textContent = 'java --version';
-    strAboutJava.appendChild(codeAboutJava);
-
-    strErrorAboutJava.innerHTML = 'If we get error like command not found, then will need to download from here: ';
-    linkJava.href = 'https://www.oracle.com/java/technologies/downloads/#jdk23-linux';
-    linkJava.textContent = 'Download Java Linux Support';
+LinuxBtn.addEventListener('click', () => {
+    strHowToJava.innerHTML = 'You can download the Java Runtime Environment(JRE) from here: ';
+    linkJava.href = 'https://www.java.com/en/download/help/linux_install.html';
     linkJava.target = '_blank';
-    strErrorAboutJava.appendChild(linkJava);
+    linkJava.textContent = 'Download for Linux';
+    strHowToJava.appendChild(linkJava);
 
-    strAboutMariaDB.innerHTML = 'First we need to check the MariaDB Version or if the MariaDB has been installed on our system, with bash and this Command: ';
-    codeAboutMariaDB.textContent = 'mysql --version';
-    strAboutMariaDB.appendChild(codeAboutMariaDB);
-    strErrorAboutMariaDB.innerHTML = 'If we get error like command not found, then will need to download from here: ';
-    linkMariaDB.href = 'https://mariadb.org/download/?t=mariadb&p=mariadb&r=11.6.2&os=Linux&cpu=x86_64&i=systemd&mirror=crete';
-    linkMariaDB.textContent = 'Download MariaDB Linux Support';
+    strHowToMariaDB.innerHTML = 'You can download the MariaDB from here: ';
+    linkMariaDB.href = 'https://mariadb.org/download/?t=mariadb&p=mariadb&r=11.4.4&os=Linux&cpu=x86_64&i=systemd&mirror=crete';
     linkMariaDB.target = '_blank';
-    strErrorAboutMariaDB.appendChild(linkMariaDB);
-});
+    linkMariaDB.textContent = 'Download for Linux';
+    strHowToMariaDB.appendChild(linkMariaDB);
 
-ArchBtn.addEventListener('click', () => {
-    strUpdateOs.innerHTML = 'To take the last Arch-Linux update will use the bash and this Command: ';
-    codeUpdateOs.textContent = 'sudo pacman -Syyuu';
-    strUpdateOs.appendChild(codeUpdateOs);
-
-    strAboutJava.innerHTML = 'First we need to check the Java Version or if the java has been installed on our system, with bash and this Command: ';
-    codeAboutJava.textContent = 'java --version';
-    strAboutJava.appendChild(codeAboutJava);
-
-    strErrorAboutJava.innerHTML = 'If we get error like command not found, then will need to download from here: ';
-    linkJava.href = 'https://www.oracle.com/java/technologies/downloads/#jdk23-linux';
-    linkJava.textContent = 'Download Java Linux Support';
-    linkJava.target = '_blank';
-    strErrorAboutJava.appendChild(linkJava);
-
-    strAboutMariaDB.innerHTML = 'First we need to check the MariaDB Version or if the MariaDB has been installed on our system, with bash and this Command: ';
-    codeAboutMariaDB.textContent = 'mysql --version';
-    strAboutMariaDB.appendChild(codeAboutMariaDB);
-    strErrorAboutMariaDB.innerHTML = 'If we get error like command not found, then will need to download from here: ';
-    linkMariaDB.href = 'https://mariadb.org/download/?t=mariadb&p=mariadb&r=11.6.2&os=Linux&cpu=x86_64&i=systemd&mirror=crete';
-    linkMariaDB.textContent = 'Download MariaDB Linux Support';
-    linkMariaDB.target = '_blank';
-    strErrorAboutMariaDB.appendChild(linkMariaDB);
+    strHowToDBeaver.innerHTML = 'You can download the DBeaver from here: ';
+    linkDBeaver.href = 'https://dbeaver.io/download/';
+    linkDBeaver.target = '_blank';
+    linkDBeaver.textContent = 'Download for Linux';
+    strHowToDBeaver.appendChild(linkDBeaver);
 });
